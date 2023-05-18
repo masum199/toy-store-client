7383fcb0ef39b87d15d3125100e21fba652ff289
unknown <24abdullahalmasum@gmail.com>
2023-05-19 01:12:14 +0600

    designing login

--- src/User/Login/Login.jsx
+++ src/User/Login/Login.jsx
@@ -83,47 +83,71 @@ const Login = () => {
 
   return (
 
-    <div className="bg-stone-500">
-      <div className="min-h-screen">
-        <div className="hero-content flex-col lg:flex-row-reverse">
-          <div className="text-center lg:text-left">
-            <h1 className="text-5xl font-bold">Login now!</h1>
-            <p className="py-6">Login to access to your products</p>
-            <div>
-              <button onClick={handleGoogleLogin} className="btn gap-2 btn-wide">
-                <FaGoogle></FaGoogle>
-                Button
-              </button>
-            </div>
-          </div>
-          <div className="flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-            <div className="card-body">
-              <form onSubmit={handleLogin}>
-                <div className="form-control">
-                  <label className="label">
-                    <span className="label-text">Email</span>
-                  </label>
-                  <input type="text" name="email" placeholder="email" className="input input-bordered" />
-                </div>
-                <div className="form-control">
-                  <label className="label">
-                    <span className="label-text">Password</span>
-                  </label>
-                  <input type="text" name="password" placeholder="password" className="input input-bordered" />
-                  <label className="label">
-                    <p className="">Don`t have an account? <Link className="font-bold" to="/register">Please Register</Link></p>
-                  </label>
-                </div>
-                <div className="form-control mt-6">
-                  <input type="submit" value="Login" className="btn btn-primary" />
-                </div>
-              </form>
-
-            </div>
-          </div>
+    <div className="bg-gradient-to-r from-teal-500 to-blue-600">
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="bg-white rounded-lg shadow-lg p-8 w-full sm:max-w-md">
+        <h1 className="text-6xl font-bold text-gray-800 mb-8">Welcome back!</h1>
+        <p className="text-lg text-gray-700 mb-10">Sign in to your account</p>
+        <div className="flex items-center justify-center space-x-4">
+          <button
+            onClick={handleGoogleLogin}
+            className="btn btn-accent btn-lg w-32 flex items-center justify-center"
+          >
+            <FaGoogle className="text-2xl" />
+          </button>
         </div>
+        <div className="flex items-center justify-center mt-6">
+          <div className="w-1/4 h-px bg-gray-400"></div>
+          <p className="text-gray-600 mx-4">OR</p>
+          <div className="w-1/4 h-px bg-gray-400"></div>
+        </div>
+        <form onSubmit={handleLogin} className="space-y-6 mt-6">
+          <div className="relative">
+            <label className="block text-lg text-gray-800 font-semibold mb-2">Email</label>
+            <input
+              type="text"
+              name="email"
+              placeholder="Your email address"
+              className="input input-bordered input-accent w-full"
+            />
+          </div>
+          <div className="relative">
+            <label className="block text-lg text-gray-800 font-semibold mb-2">Password</label>
+            <input
+              type="password"
+              name="password"
+              placeholder="Your password"
+              className="input input-bordered input-accent w-full"
+            />
+            <label className="label absolute text-sm text-gray-600 mt-2">
+              Don't have an account?{' '}
+              <Link className="font-bold" to="/register">
+                Register now
+              </Link>
+            </label>
+          </div>
+          <div className="mt-10">
+            <input
+              type="submit"
+              value="Sign In"
+              className="btn btn-primary btn-lg w-full py-4 font-semibold tracking-wider mt-6"
+            />
+          </div>
+          <div className="text-center text-gray-600 text-sm">
+            By signing in, you agree to our{' '}
+            <Link className="text-accent font-semibold">
+              Terms of Service
+            </Link>{' '}
+            and{' '}
+            <Link className="text-accent font-semibold">
+              Privacy Policy
+            </Link>
+            .
+          </div>
+        </form>
       </div>
     </div>
+  </div>
   );
 };
 
--- src/User/Registration/Registration.jsx
+++ src/User/Registration/Registration.jsx
@@ -5,11 +5,11 @@ import { updateProfile } from "firebase/auth";
 
 
 const Registration = () => {
-  const {createUser} = useContext(AuthContext)
+  const { createUser } = useContext(AuthContext)
 
 
 
-  const handleRegister = event =>{
+  const handleRegister = event => {
     event.preventDefault();
     const form = event.target;
     const name = form.name.value;
@@ -18,69 +18,80 @@ const Registration = () => {
     const password = form.password.value;
     console.log(name, photo, email, password);
 
-    createUser(email,password)
-    .then(result => {
-      console.log(result)
-      const createdUser = result.user;
-      updateProfile(createdUser,{
-        displayName: name,
-        photoURL: photo,
-       })
-    })
-    .catch(error =>{
-      console.log(error.message)
-    })
-          
+    createUser(email, password)
+      .then(result => {
+        console.log(result)
+        const createdUser = result.user;
+        updateProfile(createdUser, {
+          displayName: name,
+          photoURL: photo,
+        })
+      })
+      .catch(error => {
+        console.log(error.message)
+      })
+
   }
 
 
-    return (
-        <div className="hero min-h-screen bg-base-200">
-  <div className="hero-content flex-col lg:flex-row-reverse">
-    <div className="text-center lg:text-left">
-    </div>
-    <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-      <div className="card-body">
-
-       <form onSubmit={handleRegister}>
-       <div className="form-control">
-          <label className="label">
-            <span className="label-text">Name</span>
-          </label>
-          <input type="text" name="name" placeholder="Name" className="input input-bordered" />
-        </div>
-        <div className="form-control">
-          <label className="label">
-            <span className="label-text">Photo</span>
-          </label>
-          <input type="text" name="photo" placeholder="Photo" className="input input-bordered" />
-        </div>
-        <div className="form-control">
-          <label className="label">
-            <span className="label-text">Email</span>
-          </label>
-          <input type="text" name="email" placeholder="email" className="input input-bordered" />
-        </div>
-        <div className="form-control">
-          <label className="label">
-            <span className="label-text">Password</span>
-          </label>
-          <input type="text" name="password" placeholder="password" className="input input-bordered" />
-          <label className="label">
-            <p>Already have an account? <Link className="font-bold" to='/login'>
-            Please Login
-            </Link></p>
-          </label>
-        </div>
-        <div className="form-control mt-6">
-          <input type="submit" className="btn btn-primary" value="Register" />
-        </div>
-       </form>
+  return (
+    <div className="bg-gradient-to-r from-yellow-400 via-pink-500 to-purple-600 min-h-screen flex items-center justify-center">
+      <div className="bg-white bg-opacity-20 backdrop-filter backdrop-blur-xl rounded-lg shadow-2xl p-10 w-full sm:max-w-md">
+        <h2 className="text-4xl text-center text-white font-bold mb-8">Register</h2>
+        <form onSubmit={handleRegister} className="space-y-6">
+          <div className="relative">
+            <label className="block text-lg text-white font-semibold mb-2">Name</label>
+            <input
+              type="text"
+              name="name"
+              placeholder="Name"
+              className="input input-unique input-bordered input-opacity-80 input-accent w-full"
+            />
+          </div>
+          <div className="relative">
+            <label className="block text-lg text-white font-semibold mb-2">Photo</label>
+            <input
+              type="text"
+              name="photo"
+              placeholder="Photo"
+              className="input input-unique input-bordered input-opacity-80 input-accent w-full"
+            />
+          </div>
+          <div className="relative">
+            <label className="block text-lg text-white font-semibold mb-2">Email</label>
+            <input
+              type="text"
+              name="email"
+              placeholder="Email"
+              className="input input-unique input-bordered input-opacity-80 input-accent w-full"
+            />
+          </div>
+          <div className="relative">
+            <label className="block text-lg text-white font-semibold mb-2">Password</label>
+            <input
+              type="password"
+              name="password"
+              placeholder="Password"
+              className="input input-unique input-bordered input-opacity-80 input-accent w-full"
+            />
+            <label className="label absolute text-sm text-white mt-2">
+              Already have an account?
+              <Link className="font-bold" to="/login">
+                Please Login
+              </Link>
+            </label>
+          </div>
+          <div className="">
+            <input
+              type="submit"
+              className="btn btn-unique btn-lg w-full py-3 font-semibold tracking-wide mt-6"
+              value="Register"
+            />
+          </div>
+        </form>
       </div>
     </div>
-  </div>
-</div>
-    );
+  );
 };
 
 export default Registration;
