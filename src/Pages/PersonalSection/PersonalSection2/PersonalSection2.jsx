6bedc34b58fe76e37aa22c7c81c5c00808db3d51
unknown <24abdullahalmasum@gmail.com>
2023-05-21 04:03:15 +0600

    aaded personal1

--- src/Pages/PersonalSection/PersonalSection2/PersonalSection2.jsx
+++ src/Pages/PersonalSection/PersonalSection2/PersonalSection2.jsx
@@ -1,11 +1,25 @@
-
+import React, { useState, useEffect } from 'react';
+import image1 from '../../../assets/images/monster1.jpg';
+import image2 from '../../../assets/images/monster2.jpg';
 
 const PersonalSection2 = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  const [currentImage, setCurrentImage] = useState(image1);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentImage((prevImage) => prevImage === image1 ? image2 : image1);
+    }, 3000); // Change image every 3 seconds
+
+    return () => {
+      clearInterval(interval); // Clear interval on component unmount
+    };
+  }, []);
+
+  return (
+    <div>
+      <img className='w-96 w-full' src={currentImage} alt="Monster" />
+    </div>
+  );
 };
 
 export default PersonalSection2;
