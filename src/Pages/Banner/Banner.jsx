9bb049564210e63db4f02dbed6479f1d02f54fe0
unknown <24abdullahalmasum@gmail.com>
2023-05-21 20:52:33 +0600

    added some styles

--- src/Pages/Banner/Banner.jsx
+++ src/Pages/Banner/Banner.jsx
@@ -5,25 +5,25 @@ const Banner = () => {
   return (
     <div className="hero min-h-screen text-black back ">
     <div className="hero-content flex-col lg:flex-row-reverse ">
-      <img src={banner} className="w-1/2 rounded-lg shadow-2xl" />
+      <img src={banner} className="sty w-1/2 rounded-lg shadow-2xl" />
       <div className=''>
-        <h1 className="text-6xl font-bold"><span className='textStyle'>EXPLORE</span> THE <br/> WORLD OF TOY CARS</h1>
-        <p className="py-6  ">Experience the excitement of miniature racing with our wide selection of toy cars.</p>
+        <h1 className="text-6xl font-bold"><span className='textStyle'>EXPLORE</span> <br/><span className='text-white'> THE WORLD OF TOY CARS</span></h1>
+        <p className="py-6 text-white ">Experience the excitement of miniature racing with our wide <br/>selection of toy cars.</p>
         <div className='flex gap-10'>
           <div className=''>
-          <span>+10000</span> 
-          <br />
-          <span className='text-slate-300'>Products</span>
+          <span className='focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:focus:ring-yellow-900'>+10000</span> 
+          
+          <p className=' mb-6 mt-4 text-white'>Products</p>
           </div>
           <div>
-          <span>+500</span> 
+          <span className='focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:focus:ring-yellow-900'>+500</span> 
           <br />
-          <span className='text-slate-300'>Sellers</span>
+          <p className='mb-6 mt-4 text-white'>Sellers</p>
           </div>
           <div>
-          <span>+5000</span> 
+          <span className='focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:focus:ring-yellow-900'>+5000</span> 
           <br />
-          <span className='text-slate-300'>Reviews</span>
+          <p className='mb-6 mt-4 text-white'>Reviews</p>
           </div>
         </div>
        <div className=''>
